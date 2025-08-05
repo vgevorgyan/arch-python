9b0bdcec8aedfffc126e50941386b04162e6b3e9
Vardan Gevorgyan <vgevorgyan@vtgsoftware.com>
2025-08-05 14:08:04 +0400

    Initial commit with some setups.

--- src/steps/install_base_system.py
+++ src/steps/install_base_system.py
@@ -3,14 +3,23 @@ from ..constants import CPU
 from ..helpers.utils import debug, get_cpu, run_command_with_output
 
 
+def _should_install_lvm2():
+    for partition in config["disks"]["partitions"]:
+        if "lvm2" in partition.type:
+            return True
+
+
 def install_base_system():
     print("Installing base system ...")
     base_packages = config["general"]["base_packages"]
     match get_cpu():
         case CPU.Intel:
-            base_packages += " intel-ucode"
+            base_packages += " intel-ucode "
         case CPU.AMD:
-            base_packages += " amd-ucode"
+            base_packages += " amd-ucode "
+
+    if _should_install_lvm2():
+        base_packages += " lvm3 "
 
     debug(base_packages)
 
