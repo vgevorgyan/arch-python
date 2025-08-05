c22ff739eb7dfc6ed786244a36f370d77dbc4c9c
Vardan Gevorgyan <vgevorgyan@vtgsoftware.com>
2025-08-05 14:30:37 +0400

    Initial commit with some setups.

--- src/steps/partitioning.py
+++ src/steps/partitioning.py
@@ -1,4 +1,4 @@
-from ..config import config_disks_action
+from ..config import config, config_disks_action
 from ..constants import DiskAction
 
 
@@ -6,9 +6,12 @@ def __create_partitions():
     print("Not implemented yet")
 
 def __mount_partitions():
+    for partition in config["disks"]["partitions"]:
+        print(partition)
     print("Not implemented yet")
 
 def partitioning():
+    print("Partitioning ...")
     match config_disks_action():
         case DiskAction.Create:
             __create_partitions()
