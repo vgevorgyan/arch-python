305d72c9d1417bdfeadd6c8a2209e2fab0c8a739
Vardan Gevorgyan <vgevorgyan@vtgsoftware.com>
2025-08-07 15:30:34 +0400

    Initial commit with some setups.

--- src/steps/configure_new_system.py
+++ src/steps/configure_new_system.py
@@ -19,7 +19,7 @@ def configure_new_system():
     )
     run_chroot_command_with_output(["hwclock", "--systohc"])
     run_command("sed -i '/en_US.UTF-8/s/^#//g' /mnt/etc/locale.gen")
-    edit_file("/mnt/etc/locale.gen", "en_US.UTF-8", "#en_US.UTF-8", "en_US.UTF-8")
+    edit_file("/mnt/etc/locale.gen", "#en_US.UTF-8", "#", "")
     run_chroot_command_with_output(["locale-gen"])
     run_command('echo "LANG=en_US.UTF-8" > /mnt/etc/locale.conf', shell=True)
     run_command('echo "LANGUAGE=en_US" >> /mnt/etc/locale.conf', shell=True)
