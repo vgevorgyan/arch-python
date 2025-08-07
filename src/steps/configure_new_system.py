b89238d8525d89f4ea9c9050fb0fe5805e8bbcde
Vardan Gevorgyan <vgevorgyan@vtgsoftware.com>
2025-08-07 15:05:25 +0400

    Initial commit with some setups.

--- src/steps/configure_new_system.py
+++ src/steps/configure_new_system.py
@@ -12,16 +12,26 @@ def configure_new_system():
             "/etc/localtime",
         ]
     )
-    run_chroot_command(["hwclock", "--systohc"])
-    run_chroot_command(["sed", "-i", "'/en_US.UTF-8/s/^#//g'", "/etc/locale.gen"])
+    run_chroot_command_with_output(["hwclock", "--systohc"])
+    run_chroot_command_with_output(
+        ["sed", "-i", "'/en_US.UTF-8/s/^#//g'", "/etc/locale.gen"]
+    )
     run_chroot_command_with_output(["locale-gen"])
-    run_chroot_command('echo "LANG=en_US.UTF-8" > /etc/locale.conf', shell=True)
-    run_chroot_command('echo "LANGUAGE=en_US" > /etc/locale.conf', shell=True)
-    run_chroot_command('echo "LC_ALL=C" > /etc/locale.conf', shell=True)
-    run_chroot_command('echo "' + hostname + '" > /etc/hostname', shell=True)
-    run_chroot_command('echo "127.0.0.1    localhost" > /etc/hosts', shell=True)
-    run_chroot_command('echo "::1    localhost" > /etc/hosts', shell=True)
-    run_chroot_command(
+    run_chroot_command_with_output(
+        'echo "LANG=en_US.UTF-8" > /etc/locale.conf', shell=True
+    )
+    run_chroot_command_with_output(
+        'echo "LANGUAGE=en_US" > /etc/locale.conf', shell=True
+    )
+    run_chroot_command_with_output('echo "LC_ALL=C" > /etc/locale.conf', shell=True)
+    run_chroot_command_with_output(
+        'echo "' + hostname + '" > /etc/hostname', shell=True
+    )
+    run_chroot_command_with_output(
+        'echo "127.0.0.1    localhost" > /etc/hosts', shell=True
+    )
+    run_chroot_command_with_output('echo "::1    localhost" > /etc/hosts', shell=True)
+    run_chroot_command_with_output(
         'echo "127.0.1.1    '
         + hostname
         + ".localdomain  "
