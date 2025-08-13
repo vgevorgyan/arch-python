68013dde09d096f9c37f0e835f3c601b4e7d0bf4
Vardan Gevorgyan <vgevorgyan@vtgsoftware.com>
2025-08-13 18:22:54 +0400

    Initial commit with some setups.

--- src/main.py
+++ src/main.py
@@ -9,52 +9,21 @@ from .steps.kernel_configuration import kernel_configuration
 from .steps.partitioning import partitioning
 from .steps.user_configuration import user_configuration
 
-# TODO:  1. Validate config files
-
-# TODO: General - timezone, base system packages, drivers, desktop, username,
-# home folder, groups
-# TODO: Disks - partitions
-# TODO: Network - hostname, network config (IP, netmask, gateway or DHCP)
-
-# TODO:  2. Check Internet access for installation
-
 print("Checking network connection ...")
 if not is_connected():
     raise SystemExit("No internet connection. Please configure your network.")
-
-# TODO:  3. Configure NTP
 
 print("Enabling NTP ...")
 run_command(["timedatectl", "set-ntp", "true"])
 print("Setting timezone ...")
 run_command(["timedatectl", "set-timezone", config["general"]["timezone"]])
 
-# TODO:  4. Partitioning
-
 partitioning()
-
-# TODO:  5. Install base system
-
 install_base_system()
-
-# TODO:  6. Configure new system timezone, clock, locale and hosts
-
 configure_new_system()
-
-# TODO:  7. User configuration
-
 user_configuration()
-
-# TODO:  8. fstab generation
-
 run_command("genfstab -pU /mnt >> /mnt/etc/fstab", shell=True)
-
-# TODO:  9. Kernel configuration and grub installation
-
 kernel_configuration()
-
-# TODO: 10. Configure sudo
-
 install_packages(["sudo"])
 edit_file(
     "/mnt/etc/sudoers",
